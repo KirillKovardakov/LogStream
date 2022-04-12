1406ac602c09d812c22b47857ef74a8b9a2c6407
Kirill Kovardakov <96287721+KirillKovardakov@users.noreply.github.com>
2022-04-12 19:33:50 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+
+print("Task1 number and around numbers\n", 49, '\n', 49 - 1, '\t', 49 + 1)
+
+x = int(input())
+y = int(input())
+if x > y:
+    print("X is more than Y")
+elif x < y:
+    print("Y is more than X")
+if x > y * 3:
+    print("X is more than Y in 3 times")
+
